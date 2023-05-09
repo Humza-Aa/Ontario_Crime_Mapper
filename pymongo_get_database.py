47599bf0496870b9e45b0877544d836c84d35ab7
Humza-A <humzaaamir61@gmail.com>
2023-05-09 18:43:11 -0400

    Added: mongoDB dB for tweet storage

--- pymongo_get_database.py
+++ pymongo_get_database.py
@@ -3,22 +3,22 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-twitterPassword = os.getenv('TW_PASSWORD')
+MONGODB_PASS = os.getenv('MONGODB_PASS')
 
 def get_database():
  
    # Provide the mongodb atlas url to connect python to mongodb using pymongo
-   CONNECTION_STRING = "mongodb+srv://OCMB1:"+ twitterPassword +"@cluster0.e5wloiz.mongodb.net/?retryWrites=true&w=majority"
+   CONNECTION_STRING = "mongodb+srv://OCMB1:"+ MONGODB_PASS +"@cluster0.e5wloiz.mongodb.net/?retryWrites=true&w=majority"
  
    # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
    client = MongoClient(CONNECTION_STRING)
- 
+
+   print(client.drop_database('Toronto_Police_Crime_Report'))
    # Create the database for our example (we will use the same database throughout the tutorial
    return client['Toronto_Police_Crime_Report']
   
 # This is added so that many files can reuse the function get_database()
 if __name__ == "__main__":   
-  
    # Get the database
    dbname = get_database()
 
