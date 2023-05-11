fceccf9a2154fcb979ad45d06d4867556d2f6977
Humza-A <humzaaamir61@gmail.com>
2023-05-11 14:02:06 -0400

    Merged Ontario-Crime-Mapper-Backend_Repo

---
+++ Ontario-Crime-Mapper-Backend/index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+//Importing Routes
+const authRoute = require("./routes/auth");
+const postRoute = require("./routes/post");
+
+//connect to db
+mongoose
+  .connect(
+    process.env.MONGODB_LINK
+  )
+  .then(() => console.log("Connected to db"))
+  .catch((error) => {
+    console.log(error);
+  });
+
+mongoose.connection.on("error", (err) => {
+  console.log(err);
+});
+
+//Middleware
+app.use(express.json());
+
+//Route Middleware
+app.use("/api/newUser", authRoute);
+app.use('/api/post', postRoute);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(3000, () =>
+  console.log(`Server is running on Port: ${process.env.PORT}`)
+);
