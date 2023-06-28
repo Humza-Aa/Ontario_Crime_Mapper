cd3a79c95100036fbde182ab350e7691f130f905
Humza-A <humzaaamir61@gmail.com>
2023-06-28 16:53:19 -0400

    updated: Allowed Origin

--- Ontario-Crime-Mapper-Backend/config/allowedOrigin.js
+++ Ontario-Crime-Mapper-Backend/config/allowedOrigin.js
@@ -2,7 +2,7 @@ const allowedOrigins = [
     'https://ontario-crime-mapper-humza-aa.vercel.app',
     'https://ontario-crime-mapper-git-main-humza-aa.vercel.app',
     'https://ontario-crime-mapper.vercel.app',
-    'https://crimevue.vercel.app',
+    'https://crimevue.vercel.app/',
     // 'http://localhost:3000'
 ];
 
