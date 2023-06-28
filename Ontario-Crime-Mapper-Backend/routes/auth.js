83d629d4f1096e308ed166925d294420d3904da3
Humza-A <humzaaamir61@gmail.com>
2023-06-27 21:08:30 -0400

    Testing: deployment

--- Ontario-Crime-Mapper-Backend/routes/auth.js
+++ Ontario-Crime-Mapper-Backend/routes/auth.js
@@ -42,7 +42,7 @@ router.post("/register", async (req, res) => {
 router.post("/login", async (req, res) => {
   const resEmail = req.body.email;
   const resPass = req.body.password;
-  console.log(req.query.email)
+  console.log(req)
   const { error } = loginValidation(req.body);
   if (error) {
     return res.status(400).send(error.details[0].message);
