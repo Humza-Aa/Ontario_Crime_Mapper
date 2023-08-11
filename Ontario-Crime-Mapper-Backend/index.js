8fc34d9ab0cf866f39b6f39a81777dc87915fbc1
Humza-Aa <humzaaamir31@gmail.com>
2023-08-11 12:16:41 -0400

    CORS Updates

--- Ontario-Crime-Mapper-Backend/index.js
+++ Ontario-Crime-Mapper-Backend/index.js
@@ -39,11 +39,13 @@ app.use(express.json());
 app.use(cookieParser());
 
 
-// app.use((req, res, next) => {
-//   res.setHeader('Access-Control-Allow-Origin', 'https://crimevue.vercel.app');
-//   res.setHeader('Access-Control-Allow-Credentials', 'true'); // Allow sending cookies from frontend
-//   next();
-// });
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://crimevue.vercel.app');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
 
 
 app.get("/home", (req, res) => {
