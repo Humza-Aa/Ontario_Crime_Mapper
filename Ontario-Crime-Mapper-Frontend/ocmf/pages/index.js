443b6788cd242407e22593375557802a94263570
Humza-Aa <humzaaamir31@gmail.com>
2024-02-21 22:42:15 -0500

    Updated: Features Section Updated: Landing Page

--- Ontario-Crime-Mapper-Frontend/ocmf/pages/index.js
+++ Ontario-Crime-Mapper-Frontend/ocmf/pages/index.js
@@ -1,13 +1,10 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Footer from "../Components/Footer/Footer";
 import Header from "../Components/Header/Header";
 import MapSection from "../Components/Map_Section/MapSection";
 import App_Overview from "../Components/Opening_Sections/App_Overview/App_Overview";
 import OpeningSection from "../Components/Opening_Sections/OpeningSection";
 import TestimonialSection from "../Components/Testimonials/TestimonialSection";
-import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
-import { Link } from "@chakra-ui/react";
-import styles from "../Components/Opening_Sections/OpeningSection.module.css";
+
 export default function Home() {
   return (
     <>
