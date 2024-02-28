1da534ff01014437f4b7e55a12ba7e033769d4f2
Humza-Aa <humzaaamir31@gmail.com>
2024-02-27 22:54:50 -0500

    Background Issues

--- Ontario-Crime-Mapper-Frontend/ocmf/Provider/Theme.jsx
+++ Ontario-Crime-Mapper-Frontend/ocmf/Provider/Theme.jsx
@@ -2,20 +2,10 @@
 import { extendTheme } from '@chakra-ui/react'
 
 const theme = extendTheme({
-  styles: {
-    global: {
-      // styles for the `body`
-      body: {
-        // bg: '#1a202c',
-        // color: 'white',
-        
-      },
-    },
-  },
-  config: {
-    initialColorMode: "dark",
-    useSystemColorMode: false,
-  },
+  // config: {
+  //   initialColorMode: "dark",
+  //   useSystemColorMode: false,
+  // },
 })
 
 export default theme;
--- Ontario-Crime-Mapper-Frontend/ocmf/pages/_app.js
+++ Ontario-Crime-Mapper-Frontend/ocmf/pages/_app.js
@@ -1,12 +1,13 @@
 import "../styles/globals.css";
 import { AuthProvider } from "../context/AuthProvider";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, CSSReset } from "@chakra-ui/react";
 import { Providers } from "../Provider/Provider";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Providers>
+        <CSSReset />
         <AuthProvider>
           <Component {...pageProps} />
         </AuthProvider>
