3f7eec44e04c9586f7bcb5d5f600df44c442a000
Humza-Aa <humzaaamir31@gmail.com>
2024-02-29 14:46:14 -0500

    Updated: CORS

--- Ontario-Crime-Mapper-Backend/config/allowedOrigin.js
+++ Ontario-Crime-Mapper-Backend/config/allowedOrigin.js
@@ -3,7 +3,7 @@ const allowedOrigins = [
     'https://ontario-crime-mapper-git-main-humza-aa.vercel.app',
     'https://ontario-crime-mapper.vercel.app',
     'https://crimevue.vercel.app',
-    'http://localhost:3000',
+    // 'http://localhost:3000',
     // 'http://127.0.0.1:3000'
 ];
 
